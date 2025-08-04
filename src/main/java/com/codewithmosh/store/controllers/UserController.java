cf9270faad212f34f6491bb638870f325b33e668
Raivydas Šimėnas <raivydas.simenas@icloud.com>
2025-08-04 11:53:59 +0300

    Add MapConstruct functionality

---
+++ src/main/java/com/codewithmosh/store/controllers/UserController.java
@@ -0,0 +1,41 @@
+package com.codewithmosh.store.controllers;
+
+import com.codewithmosh.store.dtos.UserDto;
+import com.codewithmosh.store.entities.User;
+import com.codewithmosh.store.mappers.UserMapper;
+import com.codewithmosh.store.repositories.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/users")
+public class UserController {
+    private final UserRepository userRepository;
+    private UserMapper userMapper;
+
+    @GetMapping
+    public List<UserDto> getUsers() {
+        return userRepository.findAll()
+                .stream()
+                .map(userMapper::toDto)
+                .toList();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDto> getUser(@PathVariable Long id) {
+        var user = userRepository.findById(id).orElse(null);
+
+        if (user == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(userMapper.toDto(user));
+    }
+}
